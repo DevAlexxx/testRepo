6c359c7a569024a6ed03f221534e469400d48923
DevAlexPlayzxx <akfilms00@gmail.com>
2022-02-13 19:51:23 -0800

    Test Commit and Push

---
+++ testProject/src/testClass.java
@@ -0,0 +1,15 @@
+
+public class testClass {
+	
+	public static void test(boolean fire) {
+		System.out.println("Test Fire");
+		
+		return;
+	}
+
+	public static void main(String[] args) {
+		test(true);
+
+	}
+
+}
